f999fccf8f7882ec9b45e9b1b7537c5885b10303
dancarter888 <49675893+dancarter888@users.noreply.github.com>
2021-02-10 18:51:12 +1300

    added gui

--- src/Table.js
+++ src/Table.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const Table = ({countries}) => {
     return (
-        <table>
+        <table class="table-auto border-collapse p-4">
             <thead>
-                <tr>
-                    <td>Country</td>
-                    <td>Cases</td>
+                <tr className="text-6xl">
+                    <td className="p-4">Country</td>
+                    <td className="p-4">Cases</td>
                 </tr>
             </thead>
-            <tbody>
+            <tbody className="text-3xl text-gray-600 even:rotate-45">
             {countries.map((country) => {
                 return (<tr>
-                    <td>{country.name}</td>
-                    <td>{country.confirmed}</td>
+                    <td className="p-4">{country.name}</td>
+                    <td className="p-4">{country.confirmed}</td>
                 </tr>);
             })}
             </tbody>
